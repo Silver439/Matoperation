a7a9422cb26a150938b345d1dffcdd003075f673
Silver439 <72134457+Silver439@users.noreply.github.com>
2020-11-25 21:29:07 +0800

    Update and rename matrix.cpp to Matoperation.cpp

---
+++ Matoperation.cpp
@@ -0,0 +1,179 @@
+#include"Matoperation.h";
+using namespace std;
+vector<int> address;
+vector<int> counter;
+void matrix::init()
+{
+	data = new float*[row];
+	for (int i = 0; i < row; i++)
+	{
+		data[i] = new float[col] {0};
+	}
+	if (row * col != 0)
+	{
+		address.push_back((int)&(data[0][0]));
+		counter.push_back(1);
+	}
+}
+
+void matrix:: check1()
+{
+	bool b = 0;
+	for (int i = 0; i < address.size(); i++)
+	{
+		if ((int)&(data[0][0]) == address[i]) {
+			counter[i] += 1;
+			b = 1;
+			break;
+		}
+	}
+	if (b == 0) {
+		address.push_back((int)&(data[0][0]));
+		counter.push_back(1);
+	}
+}
+
+bool matrix::check2()
+{
+	int i;
+	for (i = 0; i < address.size(); i++)
+	{
+		if ((int)&(data[0][0]) == address[i]) {
+			break;
+		}
+	}
+	cout << row << "," << col << ":" << counter[i]<<endl;
+	if (counter[i] == 1) {
+		return 1;
+	}
+	else {
+		counter[i] -= 1;
+		return 0;
+	}
+}
+
+matrix::matrix(const matrix& B)
+{
+	cout << row << " " << col << endl;
+	row = B.row;
+	col = B.col;
+	data = B.data;
+	check1();
+}
+
+matrix::matrix(int m, int n)
+{
+	row = m;
+	col = n;
+	init();
+}
+
+matrix::matrix(int m, int n,float** element)
+{
+	row = m;
+	col = n;
+	data = element;
+	check1();
+}
+
+
+matrix& matrix::operator=(const matrix& B)
+{
+	row = B.row;
+	col = B.col;
+	data = B.data;
+	check1();
+	return *this;
+}
+matrix matrix::operator+(const matrix& B)
+{
+	int m = row;
+	int n = B.col;
+	matrix C(m, n);
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			C.data[i][j] = data[i][j] + B.data[i][j];
+		}
+	}
+	return C;
+}
+matrix matrix::operator-(const matrix& B)
+{
+	int m = row;
+	int n = B.col;
+	matrix C(m, n);
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			C.data[i][j] = data[i][j] - B.data[i][j];
+		}
+	}
+	return C;
+}
+matrix matrix::operator*(const matrix &B)
+{
+	matrix C(row, B.col);
+	for (int i = 0; i < row; i++)
+	{
+		for (int k = 0; k < col; k++)
+		{
+			float v = data[i][k];
+			for (int j = 0; j < B.col; j++)
+			{
+				C.data[i][j]+= v * B.data[k][j];
+			}
+		}
+	}
+	return C;
+}
+
+matrix matrix::operator*(const float& b)
+{
+	matrix C(row, col);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++)
+		{
+			C.data[i][j] = b * data[i][j];
+		}
+	}
+	return C;
+}
+
+matrix operator*(const float a, const matrix& B)
+{
+	matrix C(B.row, B.col);
+	for (int i = 0; i < B.row; i++) {
+		for (int j = 0; j < B.col; j++)
+		{
+			C.data[i][j] = a * B.data[i][j];
+		}
+	}
+	return C;
+}
+
+ostream& operator<<(ostream& os,const matrix& B)
+{
+	for (int i = 0; i < B.row; i++)
+	{
+		for (int j = 0; j < B.col; j++)
+		{
+			os << B.data[i][j];
+			if (j == B.col - 1) os << endl;
+		}
+	}
+	return os;
+}
+
+matrix::~matrix()
+{
+	if (check2()) {
+		cout <<row<<","<<col<<":"<< "Done!" << endl;
+		delete[] data;
+	}
+	else {
+		cout <<row<<","<<col<<":"<< "Wait!" << endl;
+	}
+}
